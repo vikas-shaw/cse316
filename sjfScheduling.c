ddc3e6a26cb4cc4d0fa27eaf9a700c79fd727f15
Vikas Shaw <shaw.vikas23@gmail.com>
2018-04-13 01:16:20 +0530

    Update sjfScheduling.c

--- sjfScheduling.c
+++ sjfScheduling.c
@@ -5,16 +5,18 @@ struct process{
 
 	int burst_time;
 	int waiting_time;
+	int pId;
 };
 
-void sort_process(int pQueue[], int n){
+int n;
+void sort_process(struct process pQueue[n], int n){
 
-	int temp;
+	struct process temp;
 	for(int i = 0; i<(n-1); i++){
 
 		for(int j = 0; j<(n-i-1); j++){
 
-			if(pQueue[j]>pQueue[j+1]){
+			if(pQueue[j].burst_time>pQueue[j+1].burst_time){
 			temp = pQueue[j];
 			pQueue[j] = pQueue[j+1];
 			pQueue[j+1] = temp;
@@ -25,26 +27,60 @@ void sort_process(int pQueue[], int n){
 
 int main(){
 
-	int n;
-	
 	struct process *p;
 
-	printf("Enter the number of processes\n");
+	printf("Enter the number of processes: ");
 	scanf("%d",&n);
+	printf("\n");
+	
 	p = (struct process*) malloc (n*sizeof(struct process));
+	
 	for(int i = 0; i<n; i++){
 
-		printf("Enter the burst time of process P%d: ",i);
+		(p+i)->pId = i;
+	}
+	
+	for(int i = 0; i<n; i++){
+
+		printf("Enter the burst time of process P%d: ",(p+i)->pId);
 		scanf("%d",&(p+i)->burst_time);
 	}
-	int pQueue[n];
+	
+	int waitTime[n];
+	int orderOfExecution[n];
+	int turnAround, sum = 0;
+	
+	sort_process(p,n);
+	
+	printf("\nThe sequence of process according to brust time is: ");
+	
 	for(int i = 0; i<n; i++){
 
-		pQueue[i] = (p+i)->burst_time;
+		orderOfExecution[i] = (p+i)->pId;
+		printf("P%d ",orderOfExecution[i]);
 	}
-	sort_process(pQueue,n);
+	printf("\n\n");
+	
+	waitTime[0]=0;
+	for(int i = 1; i<=n; i++){
+
+		waitTime[i] = sum + p[i-1].burst_time;
+		sum = sum + p[i-1].burst_time;	
+	}
+	
 	for(int i = 0; i<n; i++){
 
-		printf("%d ",pQueue[i]);
+		printf("Waiting time for process P%d : %d \n",orderOfExecution[i],waitTime[i]);
 	}
+	int avgWaitingTime, avgTurnaround;
+	int waitSum = 0, turnaroundSum = 0;
+	for(int i = 0; i<n; i++){
+
+		waitSum = waitSum + waitTime[i];
+		turnaroundSum = turnaroundSum + (p+i)->burst_time;
+	}
+	avgWaitingTime = waitSum/n;
+	avgTurnaround = turnaroundSum/n;
+	printf("\nAverage Waiting time is: %d\n",avgWaitingTime);
+	printf("Average Turnaround time is: %d\n",avgTurnaround);
 }
